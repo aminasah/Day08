58580131346e68f288262b668dd3ae7efcc4311d
a_sahovic565@hotmail.com <a_sahovic565@hotmail.com>
2021-03-15 23:31:12 +0100

    Day 8 finsih

---
+++ ex01/src/index.js
@@ -0,0 +1,7 @@
+//write code below this line
+function reusableFunction (){
+    console.log("Hi Padawan!");
+}
+//write code above this line
+reusableFunction();
+module.exports = reusableFunction;
